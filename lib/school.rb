09bc6061be5a0f536ec7db070acb9f2106bb64a3
Adam Nowland <acnowland@gmail.com>
2021-02-04 15:04:09 -0700

    commit tree 8be0cc15e0d7f66673dc3bfb418469e465993314

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
 # code here!
+require 'pry'
+class School
+    
+
+    def initialize name
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = [name]
+        end
+        
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        self.roster.each {|key, value| roster[key] = value.sort}
+
+    end
+
+end
+
+
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+
+binding.pry
+0
